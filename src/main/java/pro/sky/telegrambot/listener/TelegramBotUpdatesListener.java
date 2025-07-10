12c06a2f860201df33503cae5b5316c42188c44a
Andrei Gorshkov <andruhinsans@mail.ru>
2025-07-10 12:23:00 +0300

    научился обрабатывать команду /start

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,6 +3,8 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +30,13 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
+            String textMessage = update.message().text();
+            Long chatId = update.message().chat().id();
+
+            if (textMessage.equals("/start")){
+                SendMessage hiMessage = new SendMessage(chatId,"Привет");
+                SendResponse sendResponse = telegramBot.execute(hiMessage);
+            }
             // Process your updates here
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
